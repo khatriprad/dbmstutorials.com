34fea7d8e0a2f293dba09d96b1bc6b43cc306610
Pradeep Khatri <Pradeep@Khatris-MacBook-Pro.local>
2018-08-24 22:02:32 +0530

    hdfs setup

---
+++ hive/hadoop-env.sh
@@ -0,0 +1,7 @@
+
+# export JAVA_HOME=
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_141.jdk/Contents/Home
+
+# Location of Hadoop.  By default, Hadoop will attempt to determine
+export HADOOP_HOME=$HADOOP_HOME
+
